6b00c47ed72d0ae3444701df8b4843962cf53377
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-05 19:07:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,7 +50,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input.to_i - 1
+  input_to_index(input)
   if valid_move?(board,index) == true
     move(board, index, players_token = "X")
     display_board(board)
